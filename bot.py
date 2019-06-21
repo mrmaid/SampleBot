742eaf09608ad8e40389f2a447d3228512986e1d
mrmaid <42399616+mrmaid@users.noreply.github.com>
2019-06-21 17:48:47 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,26 @@
+import telebot, pprint
+bot = telebot.TeleBot('811026278:AAEcMsGsFQ0sDAYMA3hZ6pn45c00wcldP0U')
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('Привет', 'Пока')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')#, reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    #print(message.sticker)
+    bot.send_sticker(message.chat.id, message.sticker.file_id)
+    
+bot.polling()
+
